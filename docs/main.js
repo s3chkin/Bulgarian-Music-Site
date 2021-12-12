2b97cab4732c97f3432e3a6339e8e8a8c22ca2ce
s3chkin <seckins191@gmail.com>
2021-12-12 22:12:42 +0200

    added new project

---
+++ docs/main.js
@@ -0,0 +1,70 @@
+// var mysong = document.getElementById("mysong");
+// var icon = document.getElementById("icon");
+
+// icon.onclick = function() {
+//     if (mysong.paused) {
+//         mysong.play();
+//         icon.src = "./images/pause.png";
+
+//     } else {
+//         mysong.pause();
+//         icon.src = "./images/play.png";
+//     }
+// }
+
+
+var mysong2 = document.getElementById("mysong2");
+var icon2 = document.getElementById("icon2");
+
+icon2.onclick = function() {
+    if (mysong2.paused) {
+        mysong2.play();
+        icon2.src = " ./images/pause.png";
+
+    } else {
+        mysong2.pause();
+        icon2.src = " ./singers/diona.jpg ";
+    }
+}
+
+var mysong3 = document.getElementById("mysong3");
+var icon3 = document.getElementById("icon3");
+
+icon3.onclick = function() {
+    if (mysong3.paused) {
+        mysong3.play();
+        icon3.src = "./images/pause.png";
+
+    } else {
+        mysong3.pause();
+        icon3.src = "./singers/boni1.jpg ";
+    }
+}
+
+var mysong4 = document.getElementById("mysong4");
+var icon4 = document.getElementById("icon4");
+
+icon4.onclick = function() {
+    if (mysong4.paused) {
+        mysong4.play();
+        icon4.src = "./images/pause.png";
+
+    } else {
+        mysong4.pause();
+        icon4.src = "./singers/alisia1.jpg ";
+    }
+}
+
+var mysong5 = document.getElementById("mysong5");
+var icon5 = document.getElementById("icon5");
+
+icon5.onclick = function() {
+    if (mysong5.paused) {
+        mysong5.play();
+        icon5.src = "./images/pause.png";
+
+    } else {
+        mysong5.pause();
+        icon5.src = "./singers/yanitsa.jpg  ";
+    }
+}
